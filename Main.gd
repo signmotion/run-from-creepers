3b74530b890118ce9a87362c39ca5fdf6db2d1e5
Andrey Syrokomskiy <signmotion@gmail.com>
2020-03-22 19:53:08 +0200

    Removed the creeps by start a new game.

--- Main.gd
+++ Main.gd
@@ -53,7 +53,8 @@ func _on_MobTimer_timeout():
     # Set the velocity (speed & direction).
     mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
     mob.linear_velocity = mob.linear_velocity.rotated(direction)
-
+    
+    $HUD.connect("start_game", mob, "_on_start_game")
 
 
 func _on_HUD_start_game():
