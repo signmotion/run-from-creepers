6ee3fdd560368ca747830a036cddb56fa2babcbc
Andrey Syrokomskiy <signmotion@gmail.com>
2020-03-22 20:29:38 +0200

    Prepared the game for public to any device.

--- Player.gd
+++ Player.gd
@@ -6,23 +6,29 @@ signal hit
 
 export var speed = 400  # How fast the player will move (pixels/sec).
 var screen_size  # Size of the game window.
+var target = Vector2()
 
 
 func _ready():
     screen_size = get_viewport_rect().size
     hide()
-    
-    
+
+
+# Change the target whenever a touch event happens.
+func _input(event):
+    if event is InputEventScreenTouch and event.pressed:
+        target = event.position
+
+
 func _process(delta):
-    var velocity = Vector2()  # The player's movement vector.
-    if Input.is_action_pressed("ui_right"):
-        velocity.x += 1
-    if Input.is_action_pressed("ui_left"):
-        velocity.x -= 1
-    if Input.is_action_pressed("ui_down"):
-        velocity.y += 1
-    if Input.is_action_pressed("ui_up"):
-        velocity.y -= 1
+    # The player's movement vector.
+    var velocity = Vector2()
+    # Move towards the target and stop when close.
+    if position.distance_to(target) > 10:
+        velocity = (target - position).normalized() * speed
+    else:
+        velocity = Vector2()
+
     if velocity.length() > 0:
         velocity = velocity.normalized() * speed
         $AnimatedSprite.play()
@@ -30,6 +36,10 @@ func _process(delta):
         $AnimatedSprite.stop()
 
     position += velocity * delta
+    # We still need to clamp the player's position here because on devices that don't
+    # match your game's aspect ratio, Godot will try to maintain it as much as possible
+    # by creating black borders, if necessary.
+    # Without clamp(), the player would be able to move under those borders.
     position.x = clamp(position.x, 0, screen_size.x)
     position.y = clamp(position.y, 0, screen_size.y)
     
@@ -51,6 +61,7 @@ func _on_Player_body_entered(body):
 
 func start(pos):
     position = pos
+    target = pos
     $CollisionShape2D.disabled = false
     show()
     
